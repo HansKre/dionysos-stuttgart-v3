a74d948bde471fea397b155106c9bac1e9fe89dd
hans <hans.krebs@me.com>
2021-01-23 13:00:33 +0100

    Add radial gradient background, subtitle and button to landing page

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,20 +1,46 @@
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/styles';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import { makeStyles, withStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles((theme) => ({
     grow: {
         flexGrow: 1
+    },
+    paper: {
+        background: 'radial-gradient(ellipse at center, rgba(255,255,255,1) 0%, rgba(255,255,255,0.2) 80%, rgba(255,255,255,0) 100%)',
+        borderRadius: '50%',
+        padding: '7%'
+    },
+    button: {
+        marginTop: '40px'
     }
 }));
+
+const ColorButton = withStyles((theme) => ({
+    root: {
+        color: theme.palette.getContrastText('#4B8936'),
+        backgroundColor: '#4B8936',
+        '&:hover': {
+            backgroundColor: '#447c32',
+        },
+    },
+}))(Button);
 
 export default function Body() {
 
     const classes = useStyles();
 
     return (
-        <Grid item container justify='center' alignItems='center' className={classes.grow} >
-            <Typography color='primary' variant='h1'>DIONYSOS</Typography>
+        <Grid container justify='center' alignItems='center' className={classes.grow} >
+            <Paper className={classes.paper} elevation='0' >
+                <Grid item container direction='column' justify='center' alignItems='center' className={classes.grow} >
+                    <Typography color='primary' variant='h1'>DIONYSOS</Typography>
+                    <Typography color='primary' variant='h6'>AN AUTHENTIC GREEK RESTRAURANT</Typography>
+                    <ColorButton variant='contained' className={classes.button} >BOOK YOUR TABLE</ColorButton>
+                </Grid>
+            </Paper>
         </Grid>
     )
 }
