519573fae4dc52f903432edbcd0e760b3a7d64d3
hans <hans.krebs@me.com>
2021-01-25 21:48:12 +0100

    Add breakpoints to header

--- src/pages/main/Header.jsx
+++ src/pages/main/Header.jsx
@@ -1,4 +1,5 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -15,7 +16,10 @@ const useStyles = makeStyles((theme) => ({
         background: `rgba(0,0,0, ${OPACITY})`
     },
     toolBar: {
-        //
+        // creates a max-width media query that targets screen sizes up to and including the given breakpoint
+        [theme.breakpoints.down('xs')]: {
+            paddingRight: 0
+        },
     },
     menuButton: {
         marginRight: theme.spacing(2),
@@ -26,15 +30,20 @@ const useStyles = makeStyles((theme) => ({
     inheritFromTypo: {
         textDecoration: 'none',
         color: 'inherit'
-    }
+    },
 }));
 
 export default function Header() {
     const classes = useStyles();
 
+    const theme = useTheme();
+    const overSm = useMediaQuery(theme.breakpoints.up('sm'));
+    const over780 = useMediaQuery('@media (min-width:780px');
+    // outputs {xs: 0, sm: 600, md: 960, lg: 1280, xl: 1920}
+
     return (
         <AppBar position="absolute" className={classes.appBar}>
-            <Toolbar>
+            <Toolbar className={classes.toolBar}>
                 <Grid container >
                     <Grid item xs={4}>
                         <Typography color='textSecondary' variant="h4" className={classes.grow}>
@@ -42,20 +51,20 @@ export default function Header() {
                         </Typography>
                     </Grid>
                     <Grid item xs={8} container justify='flex-end' >
-                        <Grid item>
+                        {over780 && <Grid item>
                             <Button className={classes.menuButton}>
                                 <Typography color='textSecondary' variant="h6" className={classes.grow}>
                                     <HashLink smooth to="/#openingtimes" className={classes.inheritFromTypo}>Öffnungszeiten</HashLink>
                                 </Typography>
                             </Button>
-                        </Grid>
-                        <Grid item>
+                        </Grid>}
+                        {overSm && <Grid item>
                             <Button className={classes.menuButton}>
                                 <Typography color='textSecondary' variant="h6" className={classes.grow}>
                                     <HashLink smooth to="/#anfahrt" className={classes.inheritFromTypo}>Anfahrt</HashLink>
                                 </Typography>
                             </Button>
-                        </Grid>
+                        </Grid>}
                         <Grid item>
                             <Button className={classes.menuButton}>
                                 <Typography color='secondary' variant="h6" className={classes.grow}>
