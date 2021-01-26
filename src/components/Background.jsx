2a6db15e473ecfb5c678a4b840c6616eb902685e
hans <hans.krebs@me.com>
2021-01-26 13:30:30 +0100

    Reduce loading times on mobile by loading cropped image as background

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,11 +1,16 @@
 import { Paper } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-import Image from '../img/AdobeStock_304469260.jpg'; // Import using relative path
+import Image from '../img/AdobeStock_304469260.jpg';
+// Cropped with iPhone 8 Plus aspect ratio 414x736 = 1:1.78 using https://croppola.com
+import ImageCropped from '../img/AdobeStock_304469260_cropped.jpg';
 
 const useStyles = makeStyles((theme) => ({
     paperWithBackground: {
-        backgroundImage: `url(${Image})`,
+        backgroundImage: `url(${ImageCropped})`,
+        [theme.breakpoints.up('sm')]: {
+            backgroundImage: `url(${Image})`,
+        },
         backgroundSize: 'cover',
         minHeight: '100vh',
         maxWidth: '100vw'
