af3346296c079ab81c74a956adf744b4c9558d94
hans <hans.krebs@me.com>
2021-06-11 17:49:46 +0200

    Deactivate CDN usage for realtime-changes.

--- src/utils/sanityClient.js
+++ src/utils/sanityClient.js
@@ -4,5 +4,5 @@ export default sanityClient({
     projectId: process.env.REACT_APP_SANITY_PROJECTID,
     dataset: process.env.REACT_APP_SANITY_DATASET,
     apiVersion: '2021-03-25',
-    useCdn: true,
+    useCdn: false,
 });
